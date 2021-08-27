0a0fabd0d0735acee291d067fe74f02262db8ae3
CBernardi <cesarbbernardi@gmail.com>
2021-08-27 15:44:10 -0300

    commit

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -3,26 +3,22 @@ package petstore;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
 
 public class Pet {
-	// Attributes
     String uri = "https://petstore.swagger.io/v2/pet"; // Address for the Pet entity
 
-    // Methods
-    
-    public String lerJson(String caminhoJSon) throws IOException {  
+    public String readJSON(String caminhoJSon) throws IOException {  
         return new String(Files.readAllBytes(Paths.get(caminhoJSon)));
     }
 
     @Test
+    (priority = 1)
     public void postPet() throws IOException { // Adds a pet into the db based on the JSON file
-        String jsonBody = lerJson("db/pet1.json");
+        String jsonBody = readJSON("db/pet1.json");
 
         given()
             .contentType("application/json")
@@ -39,10 +35,10 @@ public class Pet {
             .body("tags.name", contains("STA training")) // in a JSON file, a square bracketed attribute is actually a list, and 'contains' is used to search for its contents
             .body("category.name", is("dog")) // in a JSON file, a non-square bracketed attribute is an object, and 'is' can be used when searching for its contents
         ;
-
     }
 
     @Test
+    (priority = 2)
     public void getPet_valid() throws IOException { // searches for a pet using the id from the JSON file
         String petId = "2508202120210825";
 
@@ -62,33 +58,40 @@ public class Pet {
     }
 
     @Test
-    public void getPet_invalid() throws IOException { // searches for a pet using invalid ID format
-        String petId = "@@@@";
+    (priority = 3)
+    public void putPet() throws IOException { // Adds a pet into the db based on the JSON file
+        String jsonBody = readJSON("db/pet2.json");
 
         given()
             .contentType("application/json")
-            .log().all()   
+            .log().all()
+            .body(jsonBody)
         .when()
-            .get(uri+"/"+petId)
+            .put(uri)
         .then()
             .log().all()
-            .statusCode(400)
+            .statusCode(200)
+            // Verifies that the content was propperly sent
+            .body("name", is("face mc doggerson"))
+            .body("status", is("unavailable"))
         ;
     }
 
     @Test
-    public void getPet_notFound() throws IOException { // searches for a pet using non-existing ID
-        String petId = "";
+    (priority = 2)
+    public void deletePet() throws IOException { // searches for a pet using the id from the JSON file
+        String petId = "2508202120210825";
 
         given()
             .contentType("application/json")
             .log().all()   
         .when()
-            .get(uri+"/"+petId)
+            .delete(uri+"/"+petId)
         .then()
             .log().all()
-            .statusCode(404)
+            .statusCode(200)
         ;
     }
+
 
 } 
